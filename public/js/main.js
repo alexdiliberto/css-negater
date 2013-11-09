67e41a3419de58052264fc47f38fb4a50de6e504
Nathan Hammond <nathan@nathanhammond.com>
2013-11-09 15:04:11 -0500

    Track the rendered parse pages.

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,10 @@
 var target = 0;
-
 $('#parse').on('submit', function(e) {
   var $this = $(this);
   var url = $('#url').val();
   target++;
   this.target = "output"+target;
   $('<iframe id="output'+target+'" name="output'+target+'"></iframe>').appendTo('body');
-
-  _gaq.push(['_trackEvent', 'negate', 'negate', url])
 });
 
 // TODO: Store and provide a way to share the generated negation CSS?
