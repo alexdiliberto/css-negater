09c56aef65c42026cf0222f41bd99a61a8d17a62
Nathan Hammond <nathan@nathanhammond.com>
2013-11-09 14:41:52 -0500

    Properly set up targeting page loads into iframes.

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,11 @@
 var target = 0;
 
 $('#parse').on('submit', function(e) {
-	// TODO: Trigger an analytics event.
-	$this = $(this);
-	$.get(this.action, $this.serialize()).done(function(response) {
-		// TODO: Allow the user to queue up multiple textarea outputs.
-		// TODO: Scroll to the top of the textarea once it has loaded.
-		// TODO: provide a one-click copy button for the textarea contents.
-		$('#output'+target).val(response).show();
-	});
-	e.preventDefault();
+  $this = $(this);
+  target++;
+  this.target = "output"+target;
+  $('<iframe id="output'+target+'" name="output'+target+'"></iframe>').appendTo('body');
+  // TODO: Trigger an analytics event.
 });
 
 // TODO: Store and provide a way to share the generated negation CSS?
