7cb88e3052402a3b24e5250ecf6650606209fde1
Nathan Hammond <nathan@nathanhammond.com>
2013-11-08 19:39:00 -0500

    Update the server to be based on Connect.

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 // https://github.com/nko4/website/blob/master/module/README.md#nodejs-knockout-deploy-check-ins
 require('nko')('Dv-pUjLBGTI4IxU3');
 
+var connect = require('connect'),
+    http = require('http');
+
+var app = connect()
+  .use(connect.favicon('public/favicon.ico'))
+  .use(connect.logger('dev'))
+  .use(connect.static('public'))
+  .use(connect.directory('public'))
+  .use(connect.cookieParser())
+  .use(connect.session({ secret: 'my secret here' }))
+  .use(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<html><body>Hello from Connect!</body></html>\n');
+  });
+
 var isProduction = (process.env.NODE_ENV === 'production');
-var http = require('http');
 var port = (isProduction ? 80 : 8000);
 
-http.createServer(function (req, res) {
-  // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
-  var voteko = '<iframe src="http://nodeknockout.com/iframe/ally" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
+http
+  .createServer(app)
+  .listen(port, function(err) {
+    if (err) { console.error(err); process.exit(-1); }
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<html><body>' + voteko + '</body></html>\n');
-}).listen(port, function(err) {
-  if (err) { console.error(err); process.exit(-1); }
+    // if run as root, downgrade to the owner of this file
+    if (process.getuid() === 0) {
+      require('fs').stat(__filename, function(err, stats) {
+        if (err) { return console.error(err); }
+        process.setuid(stats.uid);
+      });
+    }
 
-  // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0) {
-    require('fs').stat(__filename, function(err, stats) {
-      if (err) { return console.error(err); }
-      process.setuid(stats.uid);
-    });
-  }
-
-  console.log('Server running at http://0.0.0.0:' + port + '/');
-});
+    console.log('Server running at http://0.0.0.0:' + port + '/');
+  });
